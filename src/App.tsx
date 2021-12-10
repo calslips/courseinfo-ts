6ce0268e756b2945879f6a6b7d5f464ea60c5f57
Caleb Phillips <csphillips5@gmail.com>
2021-12-09 21:49:34 -0500

    9.15 add type info & replace courseParts with new definition

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,57 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Total from './components/Total';
 
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: 'normal';
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: 'groupProject';
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: 'submission';
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: 'This is the leisure course part',
+      type: 'normal'
+    },
+    {
+      name: 'Advanced',
+      exerciseCount: 7,
+      description: 'This is the hard course part',
+      type: 'normal'
     },
     {
       name: 'Using props to pass data',
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: 'groupProject'
     },
     {
       name: 'Deeper type usage',
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: 'Confusing description',
+      exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+      type: 'submission'
     }
   ];
 
